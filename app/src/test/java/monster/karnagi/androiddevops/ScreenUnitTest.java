f775397541f27a56abf669f2d5ad97c6785bafdd
Fernando Karnagi <fkarnagi@gmail.com>
2022-08-06 13:53:46 +0800

    Updated unit test cases

---
+++ app/src/test/java/monster/karnagi/androiddevops/ScreenUnitTest.java
@@ -0,0 +1,22 @@
+package monster.karnagi.androiddevops;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ScreenUnitTest {
+    @Test
+    public void validateScreenSize() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void validateFontSize() {
+        assertEquals(4, 2 + 2);
+    }
+}
--- app/src/test/java/monster/karnagi/androiddevops/ServiceUnitTest.java
+++ app/src/test/java/monster/karnagi/androiddevops/ServiceUnitTest.java
@@ -1,17 +1,22 @@
 package monster.karnagi.androiddevops;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ServiceUnitTest {
     @Test
-    public void addition_isCorrect() {
+    public void validateLoginApi() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void validateChangePasswordApi() {
         assertEquals(4, 2 + 2);
     }
 }
